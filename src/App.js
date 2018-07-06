79b75fab5f3b8071110d0fd040a2d85524d1c503
Taruni <datsabtme@gmail.com>
2018-07-06 20:48:35 +0530

    Before push

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/messages');
+    console.log(response);
+    console.log('######');
+    
+    const body = await response.json();
+
+    console.log(body);
+    
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">MESSAGES</h1>
+        </header>
+        <p className="App-intro">
+        {this.state.response}
+        </p>
+      </div>   
+    );
+  }
+}
+
+export default App;
+
+
